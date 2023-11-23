7e39311d6bb5e37dc3bfff486a636fce123c472e
Artem Zharikov <plu206@ukr.net>
2023-11-23 16:15:28 +0100

    New update 23.11 (16:15)

--- sort.py
+++ sort.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from pathlib import Path
 import re
+import zipfile
 
 images = []
 documents = []
@@ -30,12 +31,13 @@ def normalize(directory):
             my_source = str(k)
             my_dest = str(k.with_name(my_dest))
             os.rename(my_source, my_dest)
-            print(filename)
+            # print(filename)
             j = m.translate(TRANS)
             my_dest = j
             my_source = str(k.with_name(m))
             my_dest = str(k.with_name(my_dest))
             os.rename(my_source, my_dest)
+
 
 def sort_files(directory):
     normalize(directory)
@@ -48,15 +50,16 @@ def sort_files(directory):
     known_extensions = set()
     unknown_extensions = set()
 
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            file_extension = filename.split('.')[-1].lower()
+    p = Path(directory)
+    for file_path in p.glob('**/*'):
+        if file_path.is_file():
+            filename = file_path.name
+            file_extension = file_path.suffix[1:].lower()
 
             if file_extension in image_extensions:
                 images.append(filename)
                 destination = 'images'
                 known_extensions.add(file_extension)
-                images.append(filename)
             elif file_extension in video_extensions:
                 videos.append(filename)
                 destination = 'videos'
@@ -69,40 +72,59 @@ def sort_files(directory):
                 musics.append(filename)
                 destination = 'audio'
                 known_extensions.add(file_extension)
+
             elif file_extension in archive_extensions:
                 archives.append(filename)
                 destination = 'archives'
-                archive_path = os.path.join(root, filename)
-                archive_folder = os.path.join(root, os.path.splitext(filename)[0])
-                try:
-                    shutil.unpack_archive(archive_path, archive_folder)
-                except:
-                    continue
+                archive_path = str(file_path)
+                archive_folder = str(file_path.parent)
+
             else:
                 destination = 'unknown'
                 unknown_extensions.add(file_extension)
-            print(destination)
 
-            destination_directory = os.path.join(root, destination)
-            os.makedirs(destination_directory, exist_ok=True)
-            new_file_path = os.path.join(destination_directory, filename)
-            shutil.move(os.path.join(root, filename), new_file_path)
+            destination_directory = p.joinpath(destination)
+           # print(destination_directory)
+            destination_directory.mkdir(parents=True, exist_ok=True)
 
-        # Видалення порожніх папок
-        for dir_name in dirs:
-            dir_path = os.path.join(root, dir_name)
-            if not os.listdir(dir_path):
-                os.rmdir(dir_path)
+            new_file_path = destination_directory.joinpath(filename)
+           # print(new_file_path)
+            shutil.move(str(file_path), str(new_file_path))
+    #print(directory)
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for dir in dirs:
+            full_path = os.path.join(root, dir)
+            if not os.listdir(full_path):
+                os.rmdir(full_path)
+    arhive_directory = p.joinpath('archives')
+    print(arhive_directory)
+    for files in os.walk(arhive_directory):
+        for file in files[2]:
+            print(file)
+            file_without_ext = os.path.splitext(file)[0]
+            print(file_without_ext)
+            pate = str(arhive_directory) + '\\' + file
+            print(pate)
+            patne = str(arhive_directory) + "\\" +  file_without_ext
+            print(patne)
 
-    print("Відомі розширення файлів:", ', '.join(known_extensions))
-    print("Невідомі розширення файлів:", ', '.join(unknown_extensions))
-    print('Документи: ', documents)
-    print('Музика: ', musics)
-    print('Відео: ', videos)
-    print('Зображення: ', images)
+            try:
+                with zipfile.ZipFile(pate) as zip_file :
+                    zip_file.testzip()
+                    zip_file.extractall(patne)
+            except zipfile.BadZipfile:
+                continue
 
-def main():
-    sort_files(directory)
+
+
+    print("Known file extensions:", ', '.join(known_extensions))
+    print("Unknown file extensions:", ', '.join(unknown_extensions))
+    print('Documents:', documents)
+    print('Music:', musics)
+    print('Videos:', videos)
+    print('Images:', images)
+
+
 
 
 
